873770f634ad244c626568dd1defa4de6329b1d5
Marcus Hallén <marcus.peter.hallen@gmail.com>
2017-01-18 12:13:57 +0100

    Add View to the game

---
+++ view/View.java
@@ -0,0 +1,103 @@
+package view; 
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class View extends JFrame {
+	
+private JPanel topPanel,bottomPanel,centerPanel,westPanel,eastPanel; 
+private JButton[][] gameButtons; 
+	
+public View(){
+	
+	createWindow();
+	createGameButtons(4,4); 
+	
+}
+
+public void createWindow() {
+	
+	setPreferredSize(new Dimension(600,800));
+    getContentPane().setLayout(new BorderLayout());
+
+    
+    topPanel= new JPanel(); 
+    JLabel label = new JLabel(); 
+    label.setFont(new Font("TeXGyreAdventor", Font.PLAIN, 50));
+    label.setForeground(new Color(255,255,255));
+    
+    
+	topPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    topPanel.setPreferredSize(new Dimension(200, 120));
+    topPanel.setBackground(new Color(46,46,46));
+    add(topPanel,BorderLayout.NORTH); 
+    //topPanel.setBackground(new Color(0,0,0));
+	
+	centerPanel= new JPanel(); 
+    centerPanel.setLayout(new GridLayout(3,3));
+	centerPanel.setBackground(new Color(46,46,46));
+	add(centerPanel,BorderLayout.CENTER);
+	
+	westPanel= new JPanel(); 
+	westPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    westPanel.setPreferredSize(new Dimension(40, 0));
+    westPanel.setBackground(new Color(46,46,46));
+    add(westPanel,BorderLayout.WEST); 
+   
+    eastPanel= new JPanel(); 
+	eastPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+    eastPanel.setPreferredSize(new Dimension(40,0));
+    eastPanel.setBackground(new Color(46,46,46));
+    add(eastPanel,BorderLayout.EAST); 
+	
+	
+	bottomPanel= new JPanel(); 
+	bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER,100,30));
+    bottomPanel.setPreferredSize(new Dimension(0,150));
+    bottomPanel.setBackground(new Color(46,46,46));
+    add(bottomPanel,BorderLayout.SOUTH); 
+	
+	
+	pack();  
+	setLocationRelativeTo(null);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+			    
+}
+
+public void createGameButtons(int x, int y){
+	
+	gameButtons = new JButton[x][y]; 
+	
+	for(int i=0;i<gameButtons.length;i++){
+	
+	for(int j=0;j<gameButtons.length;j++){
+		
+		 gameButtons[i][j] = new JButton();
+		 gameButtons[i][j].setBackground(new Color(255,0,0));
+		 gameButtons[i][j].setFont(new Font("Tahoma",Font.PLAIN,50));
+		 gameButtons[i][j].setForeground(new Color(255,255,255));
+         // gameButtons[i][j].putClientProperty("id", k);
+         // gameButtons[i][j].setContentAreaFilled(false);
+         //gameButtons[i][j].setFocusable(false);
+         centerPanel.add(gameButtons[i][j]);     
+	}
+}	
+
+
+	
+	
+}
+	
+};
+
+
+
